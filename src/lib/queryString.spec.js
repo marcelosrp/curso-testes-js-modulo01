3fd9c053e66ecd248ede0194577fe95b3b552035
Marcelo Soares <soares.srp@gmail.com>
2021-05-31 17:17:59 -0300

    6. Refatorando com confiança

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,16 +1,12 @@
-const { queryString } = require('./queryString')
+const { queryString } = require('./queryString');
 
 describe('Object to query sting', () => {
   it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'Marcelo',
       profession: 'frontend',
-    }
+    };
 
-    expect(queryString(obj)).toBe('name=Marcelo&profession=frontend')
-  })
-})
-
-/* describe('Query string to object', () => {
-  
-}); */
+    expect(queryString(obj)).toBe('name=Marcelo&profession=frontend');
+  });
+});
