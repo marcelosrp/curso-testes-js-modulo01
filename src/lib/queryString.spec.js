b4d3f1d3028ae2f4e0d9f3898222ccab1fea65cc
Marcelo Soares <soares.srp@gmail.com>
2021-05-31 16:07:44 -0300

    4. Implementar library para gerar query strings com TDD

---
+++ src/lib/queryString.spec.js
@@ -0,0 +1,18 @@
+const { queryString } = require('./queryString')
+
+describe('Object to query sting', () => {
+  it('should create a valid query string when an object is provided', () => {
+    const obj = {
+      name: 'Marcelo',
+      profession: 'frontend'
+    }
+
+    expect(queryString(obj)).toBe(
+      'name=Marcelo&profession=frontend'
+    )
+  });
+});
+
+/* describe('Query string to object', () => {
+  
+}); */
